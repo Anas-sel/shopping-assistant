c5980ee7452fe92b8cfe9fc4c0e08d35a2924970
Anas Selmi <243770552+Anas-sel@users.noreply.github.com>
2026-01-27 10:46:47 +0100

    defined general package structure and some function placeholders

---
+++ shoppingassistant/process_data.py
@@ -0,0 +1,27 @@
+
+def filter_data(product_group_names=['Shoes']):
+    '''
+    Filters the dataset to only keep the rows corresponding to the specified
+    product group names. Filters over the column 'product_group_name'
+    for the articles.csv, transactions.csv and images.
+    Removes rows that do not have corresponding images.
+    Saves the filtered data to articles_filtered.csv and transactions_filtered.csv.
+    '''
+    pass
+
+
+def preprocess():
+    '''
+    Docstring for preprocess
+    Preprocess the data
+    '''
+    pass
+
+
+def sort_by_income(article_ids):
+    '''
+    Docstring for highest_income_products
+
+    Returns a sorted list of the article_ids by highest income
+    '''
+    pass
