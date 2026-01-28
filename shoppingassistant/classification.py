95f36bdfda6e5b4fa06a7539c216d6fab5c2763c
Meerim Burkanova <meerimburkanova@gmail.com>
2026-01-28 15:36:54 +0000

    Classifier subcategory model update

--- shoppingassistant/classification.py
+++ shoppingassistant/classification.py
@@ -1,3 +1,5 @@
+from tensorflow.keras.utils import to_categorical
+from helper_functions import preprocess_single_image, load_images_and_labels
 
 def classify_subcategory(image_path, model=None):
     """
@@ -5,7 +7,14 @@ def classify_subcategory(image_path, model=None):
     using the provided model according to the column product_type_name from articles.csv
     Return the subcategory as a string from product_type_name
     """
-    pass
+    _, _, categories = load_images_and_labels(target_column='product_type_name', num_images=1)
+    img_array = preprocess_single_image(image_path)
+
+    predictions = model.predict(img_array, verbose=0)
+    predicted_idx = np.argmax(predictions[0])
+    predicted_category = categories[predicted_idx]
+
+    return predicted_category
 
 
 
