ff613add7fcd1907ba941cd99074706ded8cd539
Anas Selmi <243770552+Anas-sel@users.noreply.github.com>
2026-02-02 16:07:30 +0100

    Added Suggestions based on most revenue item that is sold with each suggestion

--- api/fast.py
+++ api/fast.py
@@ -1,5 +1,6 @@
 # TODO: Import your package, replace this by explicit imports of what you need
 from shoppingassistant.main import suggest_articles
+from shoppingassistant.helper_functions import get_prod_name
 import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -26,13 +27,14 @@ def root():
 # Endpoint for http://localhost:8000/predict?image_path=https://assets.adidas.com/images/w_600,f_auto,q_auto/7a42ee6702ea43b49b8c3ada469e818f_9366/Ultrarun_5_Running_Shoes_Black_IE8794_HM1.jpg&top_k=5
 @app.get("/predict")
 def get_predict(image_path: str, top_k: int = 5, subcategory: str = None, gender: str = None):
-    sugg_paths = suggest_articles(image_path, top_k=top_k, subcategory=subcategory, gender=gender)
-    return [
-        {'name': Path(p).name,
-         'data': base64.b64encode(Path(p).read_bytes()).decode(),
-         'description': "Description for Image",
-         'subcategory': subcategory if subcategory else "Unknown",
-         'gender': gender if gender else "Unknown"
-         }
-        for p in sugg_paths
-    ]
+    suggestions = suggest_articles(image_path, top_k=top_k, subcategory=subcategory, gender=gender)
+    return suggestions
+    # return [
+    #     {'name': get_prod_name(p),
+    #      'data': base64.b64encode(Path(p).read_bytes()).decode(),
+    #      'description': "Description for Image",
+    #      'subcategory': subcategory if subcategory else "Unknown",
+    #      'gender': gender if gender else "Unknown"
+    #      }
+    #     for p in sugg_paths
+    # ]
--- shoppingassistant/main.py
+++ shoppingassistant/main.py
@@ -6,8 +6,10 @@ import os
 from shoppingassistant.params import *
 from shoppingassistant.classification import classify_subcategory, classify_gender
 from shoppingassistant.clustering import get_similar_items
+from shoppingassistant.suggestions import suggest_from_sales
 from tensorflow.keras.models import load_model
-from shoppingassistant.helper_functions import get_image
+from shoppingassistant.helper_functions import get_image, display_suggestions, get_prod_name
+import base64
 
 
 def load_model_class():
@@ -38,6 +40,8 @@ def suggest_articles(image_path, top_k=5, subcategory=None, gender=None):
     Returns:
         list: A list of image paths for similar images.
     """
+    assert top_k > 0, "top_k must be a positive integer"
+    assert top_k <= 10, "top_k must be less than or equal to 10 to avoid long processing times"
     image_path = get_image(image_path)
 
     if subcategory is None:
@@ -54,15 +58,31 @@ def suggest_articles(image_path, top_k=5, subcategory=None, gender=None):
 
 
     # Further processing to suggest similar items based on category_pred
-    similar_articles = get_similar_items(image_path, subcategory=subcategory, gender=gender, n=top_k)
+    # Get n+1//2 similar items and the rest from sales data
+    similar_articles = get_similar_items(image_path, subcategory=subcategory, gender=gender, n=(top_k+1)//2)
+
 
     similar_images = []
     for item in similar_articles:
         similar_images.append(item['image_path']) # or article_id
 
+    sales_suggestions = suggest_from_sales(similar_images)
+    # if len(sales_suggestions) < (top_k - len(similar_images)):  # in case not enough sales suggestions fill out with similar articles
+    suggestions = similar_images + sales_suggestions
+    suggestions = suggestions[:top_k]
     # Return image object instead of path using Image
     # put images in docker image or Drive if needed
-    return similar_images
+    # return suggestions
+    return [
+        {'name': get_prod_name(p),
+         'data': base64.b64encode(Path(p).read_bytes()).decode(),
+         'description': f"Similar item #{i+1}" if i<len(similar_images) else f"Sales based suggestion #{i+1 - len(similar_images)}",
+         'subcategory': f"Subcategory: {subcategory}",
+         'gender': f"Gender: {gender}",
+         'path': p
+         }
+        for i, p in enumerate(suggestions)
+    ]
 
 
 if __name__ == "__main__":
@@ -77,6 +97,6 @@ if __name__ == "__main__":
     image = BASE_DIR + "/raw_data/test_images/temp.jpg"
     # output = get_similar_items(image_path, n=5, subcategory='Sandals', gender='Menswear')
     # display_results(image, output)
-    similar_images = suggest_articles(image, top_k=5)
-    #display(Image(image_path))
-    print("Similar images:", similar_images)
+    suggestions = suggest_articles(image, top_k=4)
+    print("Suggested similar items:", suggestions)
+    display_suggestions(suggestions)
