7ed13f7bdaa69f10d19415cc73c2791386be3a6e
Meerim Burkanova <meerimburkanova@gmail.com>
2026-02-03 15:23:37 +0000

    Merge master fix

--- shoppingassistant/classification.py
+++ shoppingassistant/classification.py
@@ -11,12 +11,7 @@ from io import BytesIO
 # from tensorflow.keras.models import load_model
 
 
-<<<<<<< HEAD
-
-def classify_subcategory(image_path, model=None):
-=======
 def classify_subcategory(image_path, model=None, articles_df=None):
->>>>>>> master
     """
     Classifies the subcategory of the item in the image located at image_path
     using the provided model according to the column product_type_name from articles.csv
@@ -24,21 +19,13 @@ def classify_subcategory(image_path, model=None, articles_df=None):
 
     This function should load the model if not provided and perform the classification.
     """
-<<<<<<< HEAD
 
     categories = ['Boots', 'Flat shoe', 'Heels', 'Sandals', 'Slippers', 'Sneakers']
-
-    if model is None:
-        model = load_model(os.path.join(BASE_DIR, 'models', 'subcategory_classifier_best.keras'))
 
     img = load_img(image_path, target_size=(224, 224))
     img_array = img_to_array(img)
     img_array = preprocess_input(img_array)
     img_array = np.expand_dims(img_array, axis=0)
-=======
-    _, _, categories = load_images_and_labels(target_column='product_type_name', num_images=1, articles_df=articles_df)
-    img_array = preprocess_single_image(image_path)
->>>>>>> master
 
     predictions = model.predict(img_array, verbose=0)
     predicted_idx = np.argmax(predictions[0])
